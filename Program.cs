09ff6139bcca403a49a33995e9e8de4429a0ca70
Orbit Rover <orbitrover@users.noreply.github.com>
2024-06-10 17:45:19 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,12 +19,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseDeveloperExceptionPage();
+    //app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
