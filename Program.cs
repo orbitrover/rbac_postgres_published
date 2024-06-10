400a39862090473893929d651e350cb5e9d8fc31
Orbit Rover <orbitrover@users.noreply.github.com>
2024-06-10 17:39:33 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
